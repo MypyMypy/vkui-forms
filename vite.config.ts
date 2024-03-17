e21cb654299537015efc29424133dcf20a46d0e4
mypymypy <danich282@gmail.com>
2024-03-17 23:30:16 +0300

    fix: paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? 'https://mypymypy.github.io/vk-market-task/' : '/',
+  base: process.env.NODE_ENV === 'production' ? 'https://mypymypy.github.io/vk-forms-task/' : '/',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
   },
